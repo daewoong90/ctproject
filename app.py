be9470290d81069ca06a851c6629f6825ffd006f
daewoong90 <chleodnd9005@naver.com>
2025-07-29 13:44:57 +0900

    Add files via upload

--- app.py
+++ app.py
@@ -19,23 +19,35 @@ with open('./index.html', 'r', encoding='utf-8') as f:
 # Box#1(4), Box#2(1)
 col1, col2 = st.columns((4,1))
 with col1:
-    with st.expander('Content #1 (유튜브) 샘올트먼의 경고'):
+    with st.expander('Content #1 AI 유튜브'):
         url = 'https://www.youtube.com/watch?v=XyEOEBsa8I4'
-        st.info('Content..')
+        st.info('샘온트먼이 경고한 AI')
         st.video(url)
+
     with st.expander('Content #2 기후변화 예측 시뮬레이션'):
         #st.write(html, unsafe_allow_html=True)
-        htmlviewer.html(html,height=2200)
-    with st.expander('Content #3 인구 예측 시뮬레이션'):
-        st.info('Content..')
+        htmlviewer.html(html,height=1000)
 
+    with st.expander('Image Content'):
+        imgfilepath = './gpt.png'
+        st.info('')
+        st.image(imgfilepath)        
+
+    with st.expander('Content #2 기후변화 예측 시뮬레이션'):
+        #st.write(html, unsafe_allow_html=True)
+        htmlviewer.html(html,height=1000)
 
 with col2:
     with st.expander('Tips..'):
         st.info('Tips..')
+
     with st.expander('Tips..'):
-        st.info('Tips..')    
+        st.info('Tips..')
+
     with st.expander('Tips..'):
-        st.info('Tips..')    
+        st.info('Tips..')
+
+    with st.expander('Tips..'):
+        st.info('Tips..')
 st.markdown('<hr>', unsafe_allow_html=True)
 st.write('<font color="BLUE">(c)copyright. all rights reserved by skykang', unsafe_allow_html=True)
