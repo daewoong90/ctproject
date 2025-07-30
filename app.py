d628a9255c7411539029067f532c51d2b3132cb8
daewoong90 <chleodnd9005@naver.com>
2025-07-30 09:22:41 +0900

    Add files via upload

--- app.py
+++ app.py
@@ -4,7 +4,15 @@ import streamlit.components.v1 as htmlviewer
 st.title('AI project Webapp!!')
 
 with open('./index.html', 'r', encoding='utf-8') as f:
-    html = f.read()
+    html1 = f.read()
+    f.close()
+
+with open('./index2.html', 'r', encoding='utf-8') as f:
+    html2 = f.read()
+    f.close()
+
+with open('./index3.html', 'r', encoding='utf-8') as f:
+    html3 = f.read()
     f.close()
 
 #     <head>
@@ -19,23 +27,26 @@ with open('./index.html', 'r', encoding='utf-8') as f:
 # Box#1(4), Box#2(1)
 col1, col2 = st.columns((4,1))
 with col1:
-    with st.expander('Content #1 AI 유튜브'):
+    with st.expander('Video content'):
         url = 'https://www.youtube.com/watch?v=XyEOEBsa8I4'
-        st.info('샘온트먼이 경고한 AI')
+        st.info('샘올트먼이 경고한 AI')
         st.video(url)
-
-    with st.expander('Content #2 기후변화 예측 시뮬레이션'):
-        #st.write(html, unsafe_allow_html=True)
-        htmlviewer.html(html,height=1000)
 
     with st.expander('Image Content'):
         imgfilepath = './gpt.png'
-        st.info('')
-        st.image(imgfilepath)        
+        st.image(imgfilepath)      
 
-    with st.expander('Content #2 기후변화 예측 시뮬레이션'):
+    with st.expander('Content #1 [CT1] 공간 중심점 찾기'):
         #st.write(html, unsafe_allow_html=True)
-        htmlviewer.html(html,height=1000)
+        htmlviewer.html(html1,height=800)
+
+    with st.expander('Content #2 [CT2] 입지 선정하기'):
+        #st.write(html, unsafe_allow_html=True)
+        htmlviewer.html(html2,height=900)
+
+    with st.expander('Content #3 [AI-X] 기후판독기'):
+        #st.write(html, unsafe_allow_html=True)
+        htmlviewer.html(html3,height=800)
 
 with col2:
     with st.expander('Tips..'):
@@ -49,5 +60,9 @@ with col2:
 
     with st.expander('Tips..'):
         st.info('Tips..')
+
+    with st.expander('Tips..'):
+        st.info('Tips..')
+
 st.markdown('<hr>', unsafe_allow_html=True)
 st.write('<font color="BLUE">(c)copyright. all rights reserved by skykang', unsafe_allow_html=True)
